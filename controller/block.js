f628e687aef62b4c9950f32b519034cc54c69d1d
dungcui <ndse06@gmail.com>
2019-06-05 19:44:45 +0700

    no message

--- controller/block.js
+++ controller/block.js
@@ -21,7 +21,7 @@ async function Update(height) {
     if(found)
     {
         var Block = new BlockModel({height});
-        await Block.Update({ height: height , updatedAt :new Date().toISOString()},function (err) {
+        await Block.update({ height: height , updatedAt :new Date().toISOString()}).exec(function (err) {
                 if(err) {
                     // console.log(err);
                 console.log("update fail") ;
